224852ab24db4f112f0b807e7db4ef4370bd67bc
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-03-07 15:32:08 -0800

    adds styles to tic tac toe board

--- components/Board.js
+++ components/Board.js
@@ -5,7 +5,7 @@ class Board extends React.Component {
   render () {
     const { board, onClick } = this.props;
     return (
-      <div className="board">
+      <div className="board cf">
         {this.props.board.map((token,index) => 
           <Field key={index} player={token} onClick={onClick.bind(null, index)} />
         )}

c1a2269a95b34a1929bc27811bbf84b3279b1cfc
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-03-07 14:19:30 -0800

    renders field components for each pos on board

--- components/Board.js
+++ components/Board.js
@@ -5,7 +5,10 @@ class Board extends React.Component {
   render () {
     const { board, onClick } = this.props;
     return (
-      <div>
+      <div className="board">
+        {this.props.board.map((token,index) => 
+          <Field key={index} player={token} onClick={onClick.bind(null, index)} />
+        )}
       </div>
     );
   }

3b48eb787272323cc9680547a306df6ddd130209
Saturn226 <serenehopes@gmail.com>
2017-03-31 04:34:18 -0400

    Done.

---
+++ break_time.py
@@ -0,0 +1,10 @@
+import time
+import webbrowser
+
+num = 3
+
+while(num > 3):
+    time.sleep(2)
+    webbrowser.open("http://www.youtube.com")
+    num = num - 1
+
--- components/Game.js
+++ components/Game.js
@@ -6,27 +6,56 @@ const solutions = require('./solutions');
 class Game extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      board: [null, null , null, null, null, null, null, null, null],
+      turn: 'X'
+    };
 
     this.handleReset = this.handleReset.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleReset (ev) {
+    ev.preventDefault();
+    this.setState({
+        board: [null, null , null, null, null, null, null, null, null],
+        turn: 'X'
+    })
   }
 
   handleClick (i, ev) {
+    ev.preventDefault();
+    this.setState({
+      board: this.state.board.map((token, index) => index === i ? this.state.turn : token),
+      turn: this.state.turn === 'X' ? 'O': 'X'
+    })
   }
 
   getWinner () {
+    let won = solutions.find(win_combo => {
+      let board = this.state.board;
+      let pos1 = board[win_combo[0]]
+      let pos2 = board[win_combo[1]]
+      let pos3 = board[win_combo[2]]
+      return pos1 === pos2 && pos2 === pos3 && (pos1 === 'X' || pos1 === 'O')
+    })
+    return won && this.state.board[won[0]];
   }
 
   isComplete () {
+    return !!this.getWinner() || this.state.board.every(pos => { return pos !== null});
   }
+
+
 
   render () {
     return (
-      <div>
+      <div className="game">
+        <Board board={this.state.board} onClick={this.handleClick} />
+      {this.isComplete() &&
+        <Status winner={this.getWinner()} />
+      }
+        <button className="game__reset" onClick={this.handleReset}>Reset Game</button>
       </div>
     );
   }

fcf992cc39632fb9c18a889429ea12f7b29d65c8
msosborne3 <msosborne@ymail.com>
2017-04-04 22:32:23 -0500

    Done.

--- components/Field.js
+++ components/Field.js
@@ -4,7 +4,7 @@ class Field extends React.Component {
   render () {
     const { player, onClick } = this.props;
     return (
-      <button>
+      <button className="field" disabled={!!player} onClick={onClick}>{player}
       </button>
     );
   }

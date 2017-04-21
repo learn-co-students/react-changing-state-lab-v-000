b10897c78371714e886869838ce216044bdb44db
ldgarber <leah.garber88@gmail.com>
2017-04-21 17:22:05 -0400

    Done.

--- components/Board.js
+++ components/Board.js
@@ -5,7 +5,12 @@ export default class Board extends React.Component {
   render () {
     const { board, onClick } = this.props;
     return (
-      <div>
+      <div className="board">
+      { 
+        board.map((square, i) => 
+          <Field key={i} player={square} onClick={onClick.bind(null, i)} />   
+        )
+      }
       </div>
     );
   }
--- components/Game.js
+++ components/Game.js
@@ -6,27 +6,78 @@ import solutions from './solutions';
 export default class Game extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      board: [null, null, null, 
+              null, null, null, 
+              null, null, null], 
+      turn: 'X', 
+    };
 
     this.handleReset = this.handleReset.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleReset (ev) {
+    ev.preventDefault(); 
+    this.setState({
+      board: [null, null, null, 
+              null, null, null, 
+              null, null, null] 
+    }); 
   }
 
   handleClick (i, ev) {
+    ev.preventDefault(); 
+
+    var board = this.state.board; 
+    board[i] = this.state.turn; 
+    this.setState({
+      board: board,  
+    }); 
+    this.updateTurn(); 
   }
 
-  getWinner () {
+  updateTurn() {
+    var turn = this.state.turn; 
+    var newTurn = turn == "X" ? "O" : "X"; 
+    this.setState({
+      turn: newTurn,  
+    })
+  }
+
+  getWinner() {
+    const board = this.state.board; 
+    for (var i = 0; i < solutions.length; i++){
+      const [a, b, c] = solutions[i]; 
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];  
+      } 
+    }
+    return undefined;  
+  }
+
+  isWin() {
+    return (this.getWinner() != null);   
   }
 
   isComplete () {
+    const board = this.state.board; 
+    for (var i = 0; i < board.length; i++) {
+      if (board[i] == null) {
+        return false; 
+      } 
+    } 
+    return true; 
   }
 
   render () {
     return (
-      <div>
+      <div className="game">
+        <Board board={this.state.board} onClick={this.handleClick} />
+
+        <button className="game__reset" onClick={this.handleReset}>Reset</button>
+
+        { this.isComplete() ? <Status winner={this.getWinner()} /> : null}
       </div>
     );
   }

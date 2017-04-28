f2485c20df8ebd4748be04cec5956a5a3b92bf75
lizdenhup <lizdenhup@gmail.com>
2017-04-28 14:41:10 -0500

    Done.

--- components/Game.js
+++ components/Game.js
@@ -3,44 +3,59 @@ import Board from './Board';
 import Status from './Status';
 import solutions from './solutions';
 
-export default class Game extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
+const INITIAL_STATE = {
       board: [
       null, null, null,
       null, null, null,
-      null, null, null],
+      null, null, null
+      ],
       turn: 'X'
     };
+
+export default class Game extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = INITIAL_STATE;
 
     this.handleReset = this.handleReset.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleReset (event) {
+  handleReset(event) {
     event.preventDefault();
+    this.setState(INITIAL_STATE);
+  }
+
+  handleClick(i, event) {
+    event.preventDefault();
+    const board = this.state.board.slice();
+    board.splice(i, 1, this.state.turn);
+    const turn = this.state.turn === 'X' ? 'O' : 'X';
     this.setState({
-      board: [
-      null, null, null,
-      null, null, null,
-      null, null, null],
-      turn: 'X'
+      board, turn 
     });
   }
 
-  handleClick (i, ev) {
-  }
-
   getWinner () {
+    const results = solutions.map(
+      (solution) => solution.map((i) => this.state.board[i]).join('')
+    );
+    const row = results.find(
+      (result) => result === 'XXX' || result === 'OOO'
+    );
+    return row && row[0];
   }
 
   isComplete () {
+    return this.state.board.every((field) => field)
   }
 
   render () {
     return (
       <div className="game">
+        <Board board={this.state.board} onClick={this.handleClick} />
+        { this.isComplete() ? <Status winner={this.getWinner()} /> : null }
+        <button className='game__reset' onClick={this.handleReset}>Reset</button>
       </div>
     );
   }

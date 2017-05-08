adfeb2ef2bfb437e6cdf4831969ca306fa1d1f32
Valentin Rosu <alispace410@gmail.com>
2017-05-08 09:37:39 +0000

    Done.

--- components/Board.js
+++ components/Board.js
@@ -5,8 +5,16 @@ export default class Board extends React.Component {
   render () {
     const { board, onClick } = this.props;
     return (
-      <div>
+      <div className="board">
+      {
+        board.map((player, i) =>
+          <Field key={i} player={player} onClick={onClick.bind(null, i)} />
+           )
+      }
       </div>
     );
   }
 }
+
+
+//The board component is a pure component. It consists of exactly nine `Field`s and accepts the `board` prop. It renders the actual Tic Tac Toe board.

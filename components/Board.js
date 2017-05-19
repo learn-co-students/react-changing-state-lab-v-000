e681d12ec12c99ed6e40a7ac9f59ec7d3dacc77b
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-05-19 13:30:46 -0700

    Done.

--- components/Board.js
+++ components/Board.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Field from './Field';
 
 export default class Board extends React.Component {
+  
   render () {
     const { board, onClick } = this.props;
     return (
-      <div>
+      <div className="board">
+      	{board.map((item, index) => {
+      		return <Field key={index} player={item} onClick={onClick.bind(null, index)}/>
+      	})}
       </div>
     );
   }
--- components/Status.js
+++ components/Status.js
@@ -3,8 +3,17 @@ import React from 'react';
 export default class Status extends React.Component {
   render () {
     const { winner } = this.props;
+    var result;
+    if (winner === undefined){
+    	result = "Tie"
+    }else if (winner === "X"){
+    	result = "X wins"
+    }else if (winner === "O"){
+    	result = "O wins"
+    }
     return (
-      <div>
+      <div className="status">
+      {result}
       </div>
     );
   }

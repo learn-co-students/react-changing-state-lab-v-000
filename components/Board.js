c440e793b597ea931e50e7dc6548a9c2bf3f8806
walwoodr <walwoodr@gmail.com>
2017-05-24 12:41:39 -0500

    Done.

--- components/Board.js
+++ components/Board.js
@@ -4,8 +4,24 @@ import Field from './Field';
 export default class Board extends React.Component {
   render () {
     const { board, onClick } = this.props;
+
     return (
-      <div>
+      <div className="board">
+        <div className="row">
+          <Field player={board[0]} onClick={onClick.bind(null, 0)} />
+          <Field player={board[1]} onClick={onClick.bind(null, 1)} />
+          <Field player={board[2]} onClick={onClick.bind(null, 2)} />
+        </div>
+        <div className="row">
+          <Field player={board[3]} onClick={onClick.bind(null, 3)} />
+          <Field player={board[4]} onClick={onClick.bind(null, 4)} />
+          <Field player={board[5]} onClick={onClick.bind(null, 5)} />
+        </div>
+        <div className="row">
+          <Field player={board[6]} onClick={onClick.bind(null, 6)} />
+          <Field player={board[7]} onClick={onClick.bind(null, 7)} />
+          <Field player={board[8]} onClick={onClick.bind(null, 8)} />
+        </div>
       </div>
     );
   }
--- components/Status.js
+++ components/Status.js
@@ -3,8 +3,20 @@ import React from 'react';
 export default class Status extends React.Component {
   render () {
     const { winner } = this.props;
+
+    function winStatement(){
+      if (winner === "X"){
+        return "X wins"
+      } else if (winner === "O"){
+        return "O wins"
+      } else if (winner === undefined) {
+        return "Tie"
+      }
+    };
+
     return (
-      <div>
+      <div className="status">
+        {winStatement()}
       </div>
     );
   }
